c0e608d74d19eb9ecc094ca77556fac7220e810f
LINDSEY POLLAN <LPOLLAN@LIVE.COM>
2022-08-23 12:17:18 -0500

    before lunch

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ require('dotenv').config();
 
 const express = require('express');
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3002;
 
 const app = express();
+
+const data = require('./data.json');
+
+// const fs = require('fs');
+
 
 app.get('/', (req, res) => {
     res.send('hello world');
 })
 
+
+//make route to shopping list and put json file in it
+
 app.get('/shoppinglist', (req, res) => {
-    res.send
+    res.send(
+        data[1]
+    )
+})
+// fs.readFile('./data.json')
+
+
+
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`);
 })
